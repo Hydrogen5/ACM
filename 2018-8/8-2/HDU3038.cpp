e73483be94b34ad1f90c9c568a76d715297bc125
Five <1017354286@qq.com>
2018-08-02 15:20:16 +0800

    8/2

--- 2018-8/8-2/HDU3038.cpp
+++ 2018-8/8-2/HDU3038.cpp
@@ -2,45 +2,48 @@
 #include <iostream>
 using namespace std;
 int pre[200010];
-int sum[200020];
+int ranks[200020];
 int ans = 0;
 int find(int x)
 {
-    if(pre[x]!=x)
-    {
-        sum[x] += sum[pre[x]];
-        pre[x] = find(pre[x]);
-    }
-    return pre[x];
+    if (x == pre[x])
+		return x;
+	int t = pre[x];
+	pre[x] = find(pre[x]);
+	ranks[x] += ranks[t];       
+	return pre[x];
 }
 void mix(int x,int y,int s)
 {
-    int px = pre[x];
-    int py = pre[y];
+    int px = find(x);
+    int py = find(y);
     if(px==py)
     {
-        if(sum[y]!=sum[x]+s) ans++;
+        if(ranks[y]!=ranks[x]+s) ans++;
     }
     else
     {
         pre[py] = px;
-        sum[py] = sum[x] + s - sum[y];
+        ranks[py] = ranks[x] + s - ranks[y];
     }
 }
 int main()
 {
     int N,M;
-    cin >> N >> M;
-    for (int i = 1; i <= N;i++) 
+    while(cin >> N >> M)
     {
-        pre[i] = i;
-        sum[i] = 0;
+        for (int i = 0; i <= N;i++) 
+        {
+            pre[i] = i;
+            ranks[i] = 0;
+        }
+        ans = 0;
+        while(M--)
+        {
+            int a,b,s;
+            cin >> a >> b >> s;
+            mix(a, b+1, s);
+        }
+        cout << ans << endl;
     }
-    while(M--)
-    {
-        int a,b,s;
-        cin >> a >> b >> s;
-        mix(a, b, s);
-    }
-    cout << ans << endl;
 }
