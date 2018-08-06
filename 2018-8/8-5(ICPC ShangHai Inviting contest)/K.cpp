95a180d93531719846e0b4d45dd2b6c3a2c4a7c0
Five <1017354286@qq.com>
2018-08-06 12:10:43 +0800

    8/5

--- 2018-8/8-5(ICPC ShangHai Inviting contest)/K.cpp
+++ 2018-8/8-5(ICPC ShangHai Inviting contest)/K.cpp
@@ -6,7 +6,7 @@
 using namespace std;
  
  
-int T,x1,y1,x2,y2;
+int T,m,n,x2,y2;
 long long int a[110][110],b[110][110];
 long long int c[110][110];
 int main(){
@@ -14,20 +14,20 @@ int main(){
     int  T1=T;
     while(T--){
         printf("Case %d:\n",T1-T);
-        scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
-            for(int i=1;i<=x1;i++)
-                for(int j=1;j<=y1;j++) scanf("%lld",&a[i][j]);
+        scanf("%d%d%d%d",&m,&n,&x2,&y2);
+            for(int i=1;i<=m;i++)
+                for(int j=1;j<=n;j++) scanf("%lld",&a[i][j]);
             for(int i=1;i<=x2;i++)
                 for(int j=1;j<=y2;j++) scanf("%lld",&b[i][j]);   
              
-        if(x2==y1) {    
-            for(int i=1;i<=x1;i++)   
+        if(x2==n) {    
+            for(int i=1;i<=m;i++)   
                 for(int j=1;j<=y2;j++) {
                     c[i][j]=0;
-                    for(int k=1;k<=y1;k++)
+                    for(int k=1;k<=n;k++)
                         c[i][j]+=a[i][k]*b[k][j];
                 }
-            for(int i=1;i<=x1;i++){
+            for(int i=1;i<=m;i++){
                 printf("%lld",c[i][1]);
                 for(int j=2;j<=y2;j++) printf(" %lld",c[i][j]);
                 printf("\n");
