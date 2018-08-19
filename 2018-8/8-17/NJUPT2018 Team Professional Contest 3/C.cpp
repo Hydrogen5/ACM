83efbd5f8666f1f6fa6206497246550b66d226e9
Five <1017354286@qq.com>
2018-08-19 16:45:21 +0800

    8/18

--- 2018-8/8-17/NJUPT2018 Team Professional Contest 3/C.cpp
+++ 2018-8/8-17/NJUPT2018 Team Professional Contest 3/C.cpp
@@ -1,36 +1,40 @@
 //Comparing answers
+//傻逼题
+//hash?还是算什么别的优化
+//原先一次(n*n)x(n*n)矩阵乘法
+//优化过后为三次(1*n)x(n*n)
 #include <iostream>
 #include <cstdio>
 #include <cstring>
 using namespace std;
 int a1[1010][1010];
-int Right[1010][1010];
 int a2[1010][1010];
 int n;
-int mul[15][15];
-int check(int i,int j)
+int vec[1010];//压缩向量
+int mid1[1010];//a1*vec压缩结果
+int mid2[1010];//a2*vec压缩结果
+int fin1[1010];//mid1*a1压缩结果
+int check()
 {
-    int tmp=0;
-    for (int k = 1;k<=n;k++)
-    {
-        tmp += mul[a1[i][k]][a1[k][j]];
-    }
-    if(tmp==a2[i][j])
-        return 1;
-    else
-        return 0;
+    for (int i = 1;i<=n;i++)
+        vec[i] = 271931 % i;
+    for (int i = 1; i <= n;i++)
+        for (int j = 1;j<=n;j++)
+            mid1[i] += vec[j] * a1[j][i];
+    for (int i = 1; i <= n;i++)
+        for (int j = 1;j<=n;j++)
+            mid2[i] += vec[j] * a2[j][i];
+    for (int i = 1; i <= n;i++)
+        for (int j = 1;j<=n;j++)
+            fin1[i] += mid1[j] * a1[j][i];
+    for (int i = 1; i <= n;i++)
+       if(fin1[i]!=mid2[i])
+           return 0;
+    return 1;
 }
-void init()
-{
-    for (int i = 0;i<=9;i++)
-    {
-        for(int j=0;j<=9;j++)
-            mul[i][j] = i * j;
-    }
-}
+
 int main()
 {
-    init();
     while(cin>>n&&n!=0)
     {
         for(int i=1;i<=n;i++)
@@ -38,17 +42,8 @@ int main()
                 scanf("%d", &a1[i][j]);
         for(int i=1;i<=n;i++)
             for (int j = 1; j <= n;j++)
-                scanf("%d", a2[i][j]);        
-       
-        int flag = 1;
-        for(int i=1;i<=n;i++)
-            for (int j = 1; j <= n;j++)
-                if(!check(i,j))
-                    {
-                        flag = 0;
-                        break;
-                    }
-                
+                scanf("%d", &a2[i][j]);        
+        int flag = check();
         if(flag) puts("YES");
         else
             puts("NO");
